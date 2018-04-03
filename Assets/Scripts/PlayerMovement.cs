1ede67eb63566122f7e2f236826402af8023d258
Sherman Liu <shermanliu03@gmail.com>
2018-04-03 16:13:27 +0800

    Initial commit

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+	[SerializeField]
+	private float moveSpeed, min, max;
+
+	private float padding = 1;
+	// Use this for initialization
+	void Start () {
+		float zDistance = transform.position.z - Camera.main.transform.position.z;
+		Vector3 leftMax = Camera.main.ViewportToWorldPoint(new Vector3(0,0, zDistance));
+		Vector3 rightMax = Camera.main.ViewportToWorldPoint(new Vector3(1,0, zDistance));
+
+		min = leftMax.x + padding;
+		max = rightMax.x - padding;
+		
+
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		if(Input.GetKey(KeyCode.LeftArrow)) {
+			this.transform.position += new Vector3 (moveSpeed * Time.deltaTime, 0, 0);
+		}
+
+		if (Input.GetKey (KeyCode.RightArrow)) {
+			this.transform.position += new Vector3 (-moveSpeed * Time.deltaTime, 0, 0);
+		}
+		//restricting movement
+		float xPosition = transform.position.x;
+		float limitX = Mathf.Clamp(xPosition, min, max); 
+		this.transform.position = new Vector3(limitX, transform.position.y, transform.position.z);
+			
+	
+	}
+}
